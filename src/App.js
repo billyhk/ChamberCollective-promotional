bb37ebc98f4cd868b85ff9771402f5f4bc67f985
Billy <billyhkaufman@outlook.com>
2021-01-11 16:00:29 -0500

    jquery-scrollify integrated on app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import React, { useEffect } from 'react';
 import './App.scss';
+import $ from 'jquery';
+
 import Header from './Components/Header/Header';
 import Home from './Components/Home/Home';
 // import Footer from ''
 
+window.jQuery = $;
+require('jquery-scrollify');
+
 const App = () => {
+  
+	useEffect(() => {
+		$(function () {
+			$.scrollify({
+				section: 'article',
+				easing: 'easeOutExpo',
+				scrollSpeed: 1100,
+				offset: 0,
+				overflowScroll: false,
+				updateHash: true,
+				touchScroll: false,
+				setHeights: true,
+			});
+		});
+		// eslint-disable-next-line
+	}, []);
+
 	return (
 		<div className='App-Container'>
 			<Header />
