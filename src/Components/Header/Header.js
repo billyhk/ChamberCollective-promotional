33eff2de04bd3087df0529da16faba3ae0edd8fe
Billy <billyhkaufman@outlook.com>
2021-01-12 21:16:49 -0500

    added side-nav / hamburger menu

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import './Header.scss';
 import { Link } from 'react-router-dom';
 import logo from '../../Component-Images/NuCalm_Logo.png';
@@ -25,14 +26,33 @@ const Header = () => {
 	document.addEventListener('scroll', debounce(storeScroll), { passive: true });
 	storeScroll();
 
+	const [open, setOpen] = useState(false);
+
+	const toggleSide = () => {
+		setOpen(!open);
+	};
+
 	return (
 		<header className='header-container'>
 			<div className='header-left'>
-				<p className='hamburger-icon'>&#9776;</p>
+				<p className='hamburger-icon' onClick={toggleSide}>
+					&#9776;
+				</p>
 				<img src={logo} alt='nucalm-logo' className='header-logo'></img>
 			</div>
+			<nav
+				className='side-nav'
+				id={open ? 'open' : 'closed'}
+				onClick={toggleSide}>
+				<div class='sidebar-links'>
+					<Link to='#'>About</Link>
+					<Link to='#'>Members</Link>
+					<Link to='#'>The Cache</Link>
+					<Link to='#'>Services</Link>
+					<Link to='#'>Contact</Link>
+				</div>
+			</nav>
 			<div className='header-right'>
-				{' '}
 				<Link to='#'>About</Link>
 				<Link to='#'>Members</Link>
 				<Link to='#'>The Cache</Link>
