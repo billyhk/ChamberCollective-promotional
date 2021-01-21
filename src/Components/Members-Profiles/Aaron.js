1cbb2b8cd7d0128a83d7d7203ae9ee92d85814d4
Billy <billyhkaufman@outlook.com>
2021-01-20 22:58:25 -0500

    shareasale added to aaron

--- src/Components/Members-Profiles/Aaron.js
+++ src/Components/Members-Profiles/Aaron.js
@@ -246,7 +246,7 @@ const MemberAaron = ({ fullPageKey }) => {
 										<br />
 										<a
 											className='aaron-anchor'
-											href='https://nucalm.com/store/'
+											href='https://shareasale.com/r.cfm?b=1598116&u=2700721&m=100346&urllink=&afftrack='
 											target='blank'>
 											Proceed to the NuCalm Shop
 										</a>
