d58533cb6b1d06fef728718240d62d5c12187c99
Billy <billyhkaufman@outlook.com>
2021-01-22 19:03:40 -0500

    changed learn more text to address cutting off last char

--- src/Components/Contact-Form/ContactForm.js
+++ src/Components/Contact-Form/ContactForm.js
@@ -58,7 +58,7 @@ const ContactForm = ({
 			</div>
 				*/}
 			<div className='button-container'>
-				<input type='submit' className='submit-button' value='Learn More' />
+				<input type='submit' className='submit-button' value='Sign Up' />
 			</div>
 		</form>
 	);
